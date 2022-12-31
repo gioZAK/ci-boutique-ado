4c7a3b707d2d2e11fb1acdef630de720b1019447
gioZAK <giovane.zakc@gmail.com>
2022-12-31 22:06:18 +0000

    Added checkout success logic

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -113,6 +113,7 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
