4ec78633065643acdf3b417b12b21fefcc86f4f2
gioZAK <giovane.zakc@gmail.com>
2023-01-05 14:19:09 +0000

    fix settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -124,16 +124,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
+DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
     }
 
 
